bacd4b43582f57441dd2a9041ff6deaf635a97bd
ReynanEstobo <estoboreynan17@gmail.com>
2026-05-19 13:17:20 +0800

    auth fixed

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const authRoutes = require("./routers/authRoutes");
 
 dotenv.config();
 
@@ -8,9 +9,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Patient Portal running on port ${PORT}`);
+  console.log(`Patient Portal running on port ${PORT}`);
 });
